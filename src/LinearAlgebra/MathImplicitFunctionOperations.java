1c4104e8c2e100c876a3cba8273ef610be7c8e1a
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-03-23 00:05:59 +0300

    create constructor for MathImplicitFunctionOperations.java with default parameters

--- src/LinearAlgebra/MathImplicitFunctionOperations.java
+++ src/LinearAlgebra/MathImplicitFunctionOperations.java
@@ -5,55 +5,85 @@ import OtherThings.PrettyOutput;
 import java.io.*;
 import java.util.*;
 public class MathImplicitFunctionOperations extends MathBase {
-    protected final double epsilon = 1E-10;
+    protected double epsilon = 1E-10;
     protected int dimension;
     protected ArrayList<PointMultiD> points;
     protected MathImplicitFunction function;
-    public MathImplicitFunctionOperations(int dimension, String pathToPoints, MathImplicitFunction function) throws FileNotFoundException
-    {
+    public MathImplicitFunctionOperations(String pathToParametersFile, String pathToPoints, ArrayList<PointMultiD> points,
+                                          int dimension, MathImplicitFunction function) throws IOException {
         this.dimension = dimension;
-        this.points = new ArrayList<>();
-        this.readPointsFromFile(pathToPoints);
-        this.function = function;
+        if (pathToParametersFile != null)
+            this.setEpsilon(pathToParametersFile);
+        if (pathToPoints != null) {
+            this.readPointsFromFile(pathToPoints);
+        } else this.points = Objects.requireNonNullElseGet(points, ArrayList::new);
+        if (function != null) {
+            this.function = function;
+            if (!this.points.isEmpty()) {
+                for (int i = 0; i < this.points.size(); i++) {
+                    Vector x = this.getPoint(i).getVectorX();
+                    this.setPoint(i, this.calculatePoint(x));
+                }
+            }
+        } else this.function = (x) -> new PointMultiD(new Vector(this.dimension), Double.NaN);
+    }
+    public MathImplicitFunctionOperations(String pathToParametersFile, String pathToPoints, int dimension,
+                                          MathImplicitFunction function) throws IOException {
+        this(pathToParametersFile, pathToPoints, null, dimension, function);
+    }
+    public MathImplicitFunctionOperations(String pathToPoints, int dimension,
+                                          MathImplicitFunction function) throws IOException {
+        this(null, pathToPoints, dimension, function);
+    }
+    public MathImplicitFunctionOperations(String pathToParametersFile, ArrayList<PointMultiD> points, int dimension,
+                                          MathImplicitFunction function) throws IOException {
+        this(pathToParametersFile, null, points, dimension, function);
+    }
+    public MathImplicitFunctionOperations(ArrayList<PointMultiD> points, int dimension,
+                                          MathImplicitFunction function) throws IOException {
+        this(null, points, dimension, function);
+    }
+    public  MathImplicitFunctionOperations(String pathToParametersFile, String pathToPoints,
+                                           int dimension) throws IOException {
+        this(pathToParametersFile, pathToPoints, null, dimension, null);
+    }
+    public  MathImplicitFunctionOperations(String pathToPoints, int dimension) throws IOException {
+        this(null, pathToPoints, dimension);
+    }
+    public MathImplicitFunctionOperations(String pathToParametersFile, int dimension,
+                                          ArrayList<PointMultiD> points) throws IOException {
+        this(pathToParametersFile, null, points, dimension, null);
+    }
+    public MathImplicitFunctionOperations(ArrayList<PointMultiD> points, int dimension) throws IOException {
+        this(null, dimension, points);
+    }
+    public MathImplicitFunctionOperations(String pathToParametersFile) throws IOException {
+        this(pathToParametersFile, null, 1);
+    }
+    public MathImplicitFunctionOperations() throws IOException {
+        this(null);
+    }
+    public double getEpsilon() { return epsilon; }
+    public int getDimension() { return this.dimension; }
+    public ArrayList<PointMultiD> getPoints() { return this.points; }
+    public PointMultiD getPoint(int index) { return this.points.get(index); }
+    public MathImplicitFunction getFunction() { return this.function; }
 
-        for (int i = 0; i < this.points.size(); i++)
-        {
-            Vector x = this.getPoint(i).getVectorX();
-            this.setPoint(i, this.calculatePoint(x));
-        }
+    public void setEpsilon(String pathToParametersFile) throws IOException {
+        this.epsilon = super.getVariablesTable(pathToParametersFile).get("epsilon");
     }
-    public MathImplicitFunctionOperations()
-    {
-        this.dimension = 1;
-        this.points = new ArrayList<>();
-        this.function = (x) -> new PointMultiD(new Vector(this.dimension), Double.NaN);
-    }
-    public int getDimension()
-    {return this.dimension; }
-    public ArrayList<PointMultiD> getPoints()
-    { return this.points; }
-    public PointMultiD getPoint(int index)
-    { return this.points.get(index); }
-    public MathImplicitFunction getFunction()
-    { return this.function; }
-    public void setDimension(int dimension)
-    { this.dimension = dimension; }
-    public void setPoints(ArrayList<PointMultiD> points)
-    { this.points = points; }
-    public void setPoint(int index, PointMultiD point)
-    { this.points.set(index, point); }
-    public void setFunction(MathImplicitFunction function)
-    { this.function = function; }
-    public void addPoint(PointMultiD point)
-    { this.points.add(point); }
+    public void setDimension(int dimension) { this.dimension = dimension; }
+    public void setPoints(ArrayList<PointMultiD> points) { this.points = points; }
+    public void setPoint(int index, PointMultiD point) { this.points.set(index, point); }
+    public void setFunction(MathImplicitFunction function) { this.function = function; }
+    public void addPoint(PointMultiD point) { this.points.add(point); }
     public void addPoints(ArrayList<PointMultiD> points)
     {
         for (PointMultiD point : points)
             this.addPoint(point);
         this.sortPoints();
     }
-    public void sortPoints()
-    { this.points.sort(new PointNormComparator()); }
+    public void sortPoints() { this.points.sort(new PointNormComparator()); }
     @Override
     public boolean equals(Object obj)
     { return super.equals(obj); }
