3882de7e99b3bd9dfe15de53bb0140e4412abe05
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-10 18:35:03 +0300

    add constructor for Pair.java add fields Pair.first, Pair.second and toString method

--- src/OtherThings/Pair.java
+++ src/OtherThings/Pair.java
@@ -1,5 +1,14 @@
 package OtherThings;
 
 public class Pair<T1, T2> {
-
+    protected T1 first;
+    protected T2 second;
+    public Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+    @Override
+    public String toString() {
+        return this.first.toString() + ", " + this.second.toString();
+    }
 }
