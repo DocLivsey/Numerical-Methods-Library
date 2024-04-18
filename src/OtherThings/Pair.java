dfc279fe62e5c84cbbfdecf4c447d29447ea29d4
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-18 12:21:01 +0300

    add new helper class to contains data in pair data structure and adds into it all default methods for default java class

--- src/OtherThings/Pair.java
+++ src/OtherThings/Pair.java
@@ -7,8 +7,24 @@ public class Pair<T1, T2> {
         this.first = first;
         this.second = second;
     }
+    public T1 getFirst() {
+        return first;
+    }
+    public T2 getSecond() {
+        return second;
+    }
+    public void setFirst(T1 first) {
+        this.first = first;
+    }
+    public void setSecond(T2 second) {
+        this.second = second;
+    }
     @Override
     public String toString() {
-        return this.first.toString() + ", " + this.second.toString();
+        if (first == null)
+            return "Undefined " + second.toString();
+        else if (second == null)
+            return this.first + " Undefined";
+        return this.first + ", " + this.second;
     }
 }
