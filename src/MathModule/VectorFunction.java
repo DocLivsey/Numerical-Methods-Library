2fc9eadecd9184f5eafa595e59403692c48372ec
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-19 22:36:22 +0300

    change validation input method and overrided abstract methods

--- src/MathModule/VectorFunction.java
+++ src/MathModule/VectorFunction.java
@@ -1,14 +1,15 @@
 package MathModule;
 
-import MathModule.Abstract.Vector;
+import MathModule.Abstract.AbstractVector;
 import MathModule.LinearAlgebra.*;
+import MathModule.LinearAlgebra.Vector;
 import OtherThings.PrettyOutput;
 import Parsers.InputStreamParser;
 
 import java.io.*;
 import java.util.*;
 
-public class VectorFunction extends Vector<MathImplicitFunctionOperations> {
+public class VectorFunction extends AbstractVector<MathImplicitFunctionOperations> {
     public VectorFunction(String pathToSettingsFile, ArrayList<MathImplicitFunctionOperations> mathImplicitFunctions)
             throws ReflectiveOperationException, IOException {
         if (pathToSettingsFile != null && !pathToSettingsFile.isEmpty()) {
@@ -25,53 +26,112 @@ public class VectorFunction extends Vector<MathImplicitFunctionOperations> {
             throws IOException, ReflectiveOperationException {
         this(null);
     }
-    public void validateAbstractMethodsInput(ArrayList<Object> args) throws IOException {
-        if (!InputStreamParser.isClassesInListAtOnce(args, MathImplicitFunction.class) &&
-                !InputStreamParser.isClassesInListAtOnce(args, PointMultiD.class))
-            throw new RuntimeException(PrettyOutput.RED_UNDERLINED + "Ошибка! Неверное количество переданных аргументов\n" +
-                    "Ожидалось на вход:\n" + PrettyOutput.CHOOSE + "class: " + PrettyOutput.COMMENT +
-                    "MathModule.LinearAlgebra.Vector<MathImplicitFunction>" + PrettyOutput.CHOOSE + " Описание: "
-                    + PrettyOutput.COMMENT + "вектор с которым суммируем\n" + PrettyOutput.CHOOSE + "class: " +
-                    PrettyOutput.COMMENT + "MathModule.LinearAlgebra.PointMultiD" + PrettyOutput.CHOOSE + " Описание: "
-                    + PrettyOutput.COMMENT + "точка в которой суммируем вектора" + PrettyOutput.RESET);
+    public void validateAbstractMethodsInput(Integer mode, Object... arguments) throws IOException {
+        switch (mode) {
+            case 1:
+                if (!InputStreamParser.isClassesInListAtOnce(Arrays.stream(arguments).toList(), MathImplicitFunction.class) &&
+                        !InputStreamParser.isClassesInListAtOnce(Arrays.stream(arguments).toList(), PointMultiD.class))
+                    throw new RuntimeException(PrettyOutput.ERROR_UNDERLINED + "Ошибка! Неверное количество переданных аргументов\n" +
+                            "Ожидалось на вход:\n" + PrettyOutput.CHOOSE + "class: " + PrettyOutput.COMMENT + VectorFunction.class +
+                            PrettyOutput.CHOOSE + " Описание: " + PrettyOutput.COMMENT + "вектор с которым суммируем\n" +
+                            PrettyOutput.CHOOSE + "class: " + PrettyOutput.COMMENT + PointMultiD.class + PrettyOutput.CHOOSE +
+                            " Описание: " + PrettyOutput.COMMENT + "точка в которой суммируем вектора" + PrettyOutput.RESET);
+            case 2:
+                if (!InputStreamParser.isClassesInListAtOnce(Arrays.stream(arguments).toList(), PointMultiD.class) &&
+                        !InputStreamParser.isClassesInListAtOnce(Arrays.stream(arguments).toList(), Number.class))
+                    throw new RuntimeException(PrettyOutput.ERROR_UNDERLINED + "Ошибка! Неверное количество переданных аргументов\n" +
+                            "Ожидалось на вход:\n" + PrettyOutput.CHOOSE + "class: " + PrettyOutput.COMMENT + Number.class +
+                            PrettyOutput.CHOOSE + " Описание: " + PrettyOutput.COMMENT + "скаляр на который умножаем вектор\n" +
+                            PrettyOutput.CHOOSE + "class: " + PrettyOutput.COMMENT + PointMultiD.class + PrettyOutput.CHOOSE +
+                            " Описание: " + PrettyOutput.COMMENT + "точка в которой умножаем на скаляр" + PrettyOutput.RESET);
+            default:
+                throw new RuntimeException(PrettyOutput.RED_BRIGHT +  "Вероятно вы ошиблись с режимом валидации " +
+                        "входных данных для абстрактных методов\n" + PrettyOutput.YELLOW_BOLD + "Доступно два режима " +
+                        "валидации:\n1 - вектор функция и точка\n2 - скаляр и точка" + PrettyOutput.RESET);
+        }
     }
     @Override
-    public Vector<? extends Number> add(ArrayList<Object> args)
+    public AbstractVector<? extends Number> add(Object... arguments)
             throws ReflectiveOperationException, IOException {
-        try {
-            validateAbstractMethodsInput(args);
-        } catch (IOException ignored) {}
+        validateAbstractMethodsInput(1, arguments);
         PointMultiD inPoint = new PointMultiD();
-        ArrayList<MathImplicitFunctionOperations> mathImplicitFunction = new ArrayList<>();
-        for (var arg : args) {
+        ArrayList<MathImplicitFunctionOperations> addFunctions = new ArrayList<>();
+        for (var arg : arguments) {
             if (arg.getClass() == PointMultiD.class)
                 inPoint = (PointMultiD) arg;
             if (arg.getClass() == ArrayList.class)
-                mathImplicitFunction.addAll((ArrayList<MathImplicitFunctionOperations>) arg);
+                addFunctions.addAll((ArrayList<MathImplicitFunctionOperations>) arg);
             else if (arg.getClass() == MathImplicitFunctionOperations.class)
-                mathImplicitFunction.add((MathImplicitFunctionOperations) arg);
+                addFunctions.add((MathImplicitFunctionOperations) arg);
         }
-        MathModule.LinearAlgebra.Vector result = new MathModule.LinearAlgebra.Vector();
-        for (var functionOperations : mathImplicitFunction) {
-            ArrayList<Object> arg = new ArrayList<>();
-            arg.add(functionOperations.calculatePoint(inPoint.getVectorX()).getVectorX());
-            result.add(arg);
+        Vector thisVector = new Vector();
+        Vector addVector = new Vector();
+        for (int i = 0; i < addFunctions.size(); i++) {
+            thisVector.setElementAt(this.vector.get(i).calculatePoint(inPoint.getVectorX()).getY(), i);
+            addVector.setElementAt(addFunctions.get(i).calculatePoint(inPoint.getVectorX()).getY(), i);
         }
-        return result;
+        return thisVector.add(addVector);
     }
     @Override
-    public Vector<? extends Number> subtraction(ArrayList<Object> args)
+    public AbstractVector<? extends Number> subtraction(Object... arguments)
             throws ReflectiveOperationException, IOException {
-        return null;
+        validateAbstractMethodsInput(1, arguments);
+        PointMultiD inPoint = new PointMultiD();
+        ArrayList<MathImplicitFunctionOperations> subFunctions = new ArrayList<>();
+        for (var arg : arguments) {
+            if (arg.getClass() == PointMultiD.class)
+                inPoint = (PointMultiD) arg;
+            if (arg.getClass() == ArrayList.class)
+                subFunctions.addAll((ArrayList<MathImplicitFunctionOperations>) arg);
+            else if (arg.getClass() == MathImplicitFunctionOperations.class)
+                subFunctions.add((MathImplicitFunctionOperations) arg);
+        }
+        Vector thisVector = new Vector();
+        Vector subVector = new Vector();
+        for (int i = 0; i < subFunctions.size(); i++) {
+            thisVector.setElementAt(this.vector.get(i).calculatePoint(inPoint.getVectorX()).getY(), i);
+            subVector.setElementAt(subFunctions.get(i).calculatePoint(inPoint.getVectorX()).getY(), i);
+        }
+        return thisVector.subtraction(subVector);
+
     }
     @Override
-    public double scalarMultiply(ArrayList<Object> args) {
-        return 0;
+    public double scalarMultiply(Object... arguments) throws IOException, ReflectiveOperationException {
+        validateAbstractMethodsInput(1, arguments);
+        PointMultiD inPoint = new PointMultiD();
+        ArrayList<MathImplicitFunctionOperations> scalarMultiplyFunctions = new ArrayList<>();
+        for (var arg : arguments) {
+            if (arg.getClass() == PointMultiD.class)
+                inPoint = (PointMultiD) arg;
+            if (arg.getClass() == ArrayList.class)
+                scalarMultiplyFunctions.addAll((ArrayList<MathImplicitFunctionOperations>) arg);
+            else if (arg.getClass() == MathImplicitFunctionOperations.class)
+                scalarMultiplyFunctions.add((MathImplicitFunctionOperations) arg);
+        }
+        Vector thisVector = new Vector();
+        Vector scalarMultiplyVector = new Vector();
+        for (int i = 0; i < scalarMultiplyFunctions.size(); i++) {
+            thisVector.setElementAt(this.vector.get(i).calculatePoint(inPoint.getVectorX()).getY(), i);
+            scalarMultiplyVector.setElementAt(scalarMultiplyFunctions.get(i).calculatePoint(inPoint.getVectorX()).getY(), i);
+        }
+        return thisVector.scalarMultiply(scalarMultiplyVector);
     }
     @Override
-    public Vector<? extends Number> constMultiply(
-            ArrayList<Object> args) {
-        return null;
+    public AbstractVector<? extends Number> constMultiply(Object... arguments)
+            throws IOException, ReflectiveOperationException {
+        validateAbstractMethodsInput(2, arguments);
+        double constant = 1;
+        PointMultiD inPoint = new PointMultiD();
+        for (var arg : arguments) {
+            if (arg.getClass() == Double.class)
+                constant = (double) arg;
+            if (arg.getClass() == PointMultiD.class)
+                inPoint = (PointMultiD) arg;
+        }
+        Vector thisVector = new Vector();
+        for (var function : this.vector)
+            thisVector.addElement(function.calculatePoint(inPoint.getVectorX()).getY());
+        return thisVector.constMultiply(constant);
     }
 
 }
